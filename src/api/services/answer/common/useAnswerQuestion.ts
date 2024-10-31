81dee43ff514001fc81b09086c3c380cd1ef25c0
Solbin Jung <jsolbin0724@gmail.com>
2024-11-01 00:26:02 +0900

    refactor: answerRandomQuestion 훅 내부 함수로 통합 및 파일 구조 개선

--- src/api/services/answer/common/useAnswerQuestion.ts
+++ src/api/services/answer/common/useAnswerQuestion.ts
@@ -1,9 +1,22 @@
 import { useMutation } from '@tanstack/react-query'
 
-import { answerRandomQuestion } from '.'
+import { authorizationInstance } from '@/api/instance'
 
 type AnswerQuestionParam = {
   friendId: number
+}
+
+type AnswerResponse = {
+  message: string
+}
+
+const answerRandomQuestion = async ({
+  friendId,
+}: AnswerQuestionParam): Promise<AnswerResponse> => {
+  const response = await authorizationInstance.post('/api/answer/common', {
+    friendId,
+  })
+  return response.data
 }
 
 export const useAnswerQuestion = ({ friendId }: AnswerQuestionParam) => {
