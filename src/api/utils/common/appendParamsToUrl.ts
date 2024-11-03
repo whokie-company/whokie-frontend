e7936dcf96106fc5f16436d6c91006ba895e5111
ajin <aj5210@naver.com>
2024-11-03 14:41:20 +0900

    Feat: appendParamsToUrl 유틸 함수 추가

---
+++ src/api/utils/common/appendParamsToUrl.ts
@@ -0,0 +1,18 @@
+export function appendParamsToUrl(
+  url: string,
+  params: Record<string, string | number | boolean | string[]>
+) {
+  const searchParams = new URLSearchParams()
+
+  Object.entries(params).forEach(([key, value]) => {
+    if (Array.isArray(value)) {
+      value.forEach((item) => {
+        searchParams.append(key, item.toString())
+      })
+    } else if (value) {
+      searchParams.append(key, value.toString())
+    }
+  })
+
+  return `${url}?${searchParams}`
+}
