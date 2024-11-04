282552aeb5f6740ce1df904866c5a9ba0623db5e
ajin <aj5210@naver.com>
2024-11-04 18:50:03 +0900

    Rename: group api 구조 수정

---
+++ src/api/services/group/group.api.ts
@@ -0,0 +1,37 @@
+import { useSuspenseInfiniteQuery } from '@tanstack/react-query'
+
+import { authorizationInstance } from '@/api/instance'
+import { getPagingPath } from '@/api/utils/common/getPagingPath'
+import { Group, PagingRequestParams, PagingResponse } from '@/types'
+
+type GroupResponse = PagingResponse<Omit<Group, 'groupDescription'>[]>
+
+const getGroupPaging = async (params: PagingRequestParams) => {
+  const { data } = await authorizationInstance.get<GroupResponse>(
+    getPagingPath('/api/group/my', params)
+  )
+
+  return {
+    groups: data.content,
+    nextPageToken:
+      data.page !== data.totalPages ? (data.page + 1).toString() : undefined,
+  }
+}
+
+interface GroupPagingProps extends PagingRequestParams {
+  initPageToken?: string
+}
+
+export const useGroupPaging = ({
+  size = 10,
+  sort,
+  initPageToken,
+}: GroupPagingProps) => {
+  return useSuspenseInfiniteQuery({
+    queryKey: ['groups'],
+    queryFn: ({ pageParam = initPageToken }) =>
+      getGroupPaging({ page: pageParam, size, sort }),
+    initialPageParam: initPageToken,
+    getNextPageParam: (lastPage) => lastPage.nextPageToken,
+  })
+}
