333a8daa7fa2c9f962409488a8048f8382f965b5
anheejeong <eyrt6973@naver.com>
2024-11-04 20:47:19 +0900

    Feat: 프로필 질문 content 추가

--- src/stores/selected-question.ts
+++ src/stores/selected-question.ts
@@ -2,12 +2,18 @@ import { create } from 'zustand'
 
 interface SelectedQuestionProps {
   questionId: number | undefined
+  questionContent: string | undefined
   setQuestionId: (questionId: number | undefined) => void
+  setQuestionContent: (questionContent: string | undefined) => void
 }
 
 export const useSeletedQuestionStore = create<SelectedQuestionProps>((set) => ({
   questionId: undefined,
+  questionContent: undefined,
   setQuestionId: (questionId) => {
     set({ questionId })
+  },
+  setQuestionContent: (questionContent) => {
+    set({ questionContent })
   },
 }))
