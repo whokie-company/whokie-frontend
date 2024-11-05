79eb2acb8b06d80c7dfcf8649322b8ffedc27773
ajin <aj5210@naver.com>
2024-11-06 02:29:09 +0900

    Refactor(create-group): 그룹 생성 페이지 컴포넌트 분리

--- src/pages/CreateGroupPage/index.tsx
+++ src/pages/CreateGroupPage/index.tsx
@@ -1,48 +1,21 @@
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { BiError } from 'react-icons/bi'
+import { BiCheckCircle, BiError } from 'react-icons/bi'
 import { useNavigate } from 'react-router-dom'
 
-import {
-  Button,
-  Flex,
-  Input,
-  Text,
-  Textarea,
-  useDisclosure,
-} from '@chakra-ui/react'
+import { Flex, useDisclosure } from '@chakra-ui/react'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useMutation } from '@tanstack/react-query'
-import { z } from 'zod'
 
 import { queryClient } from '@/api/instance'
 import {
   CreateGroupRequestBody,
   createGroup,
 } from '@/api/services/group/group.api'
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-} from '@/components/Form'
 import { AlertModal } from '@/components/Modal/AlertModal'
+import { CreateGroupFields, CreateGroupSchema } from '@/schema/create-group'
 
-import { ImageInput } from './ImageInput'
-
-const CreateGroupSchema = z.object({
-  groupName: z
-    .string()
-    .min(1, { message: '그룹명을 입력해주세요' })
-    .max(10, { message: '그룹명은 10자 이내로 작성해주세요' }),
-  groupDescription: z
-    .string()
-    .min(1, { message: '그룹 설명을 입력해주세요' })
-    .max(30, { message: '그룹 설명은 30자 이내로 작성해주세요' }),
-})
-
-type CreateGroupFields = z.infer<typeof CreateGroupSchema>
+import { CreateGroupForm } from './CreateGroupForm'
 
 export default function CreateGroupPage() {
   const form = useForm<CreateGroupFields>({
@@ -87,80 +60,7 @@ export default function CreateGroupPage() {
       justifyContent="center"
       height="full"
     >
-      <Form {...form}>
-        <form>
-          <Flex
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="center"
-            gap={6}
-          >
-            <FormField
-              control={form.control}
-              name="groupName"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input
-                      value={field.value}
-                      onChange={field.onChange}
-                      placeholder="그룹명을 입력해주세요"
-                      size="lg"
-                      width="12rem"
-                      variant="unstyled"
-                      focusBorderColor="brown.300"
-                      borderColor="brown.200"
-                      textAlign="center"
-                      fontWeight="bold"
-                      _placeholder={{ color: 'black.500' }}
-                    />
-                    <FormDescription textAlign="center">
-                      (10자 이내)
-                    </FormDescription>
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <ImageInput />
-            <FormField
-              control={form.control}
-              name="groupDescription"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Textarea
-                      value={field.value}
-                      onChange={field.onChange}
-                      placeholder="그룹 설명을 입력해주세요"
-                      width="16rem"
-                      size="sm"
-                      textAlign="center"
-                      border="none"
-                      minHeight="3.5rem"
-                    />
-                    <FormDescription textAlign="center">
-                      (30자 이내)
-                    </FormDescription>
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <Flex flexDirection="column" gap={1}>
-              <Text fontSize="small" color="text_description">
-                그룹 정보는 그룹 개설 후에도 변경할 수 있어요
-              </Text>
-              <Button
-                colorScheme="brown"
-                width="full"
-                type="submit"
-                onClick={onClickSumbitButton}
-              >
-                그룹 만들기
-              </Button>
-            </Flex>
-          </Flex>
-        </form>
-      </Form>
+      <CreateGroupForm form={form} onClickSumbitButton={onClickSumbitButton} />
       <AlertModal
         isOpen={errorAlert.isOpen}
         onClose={errorAlert.onClose}
@@ -174,7 +74,7 @@ export default function CreateGroupPage() {
           successAlert.onClose()
           navigate('/')
         }}
-        icon={<BiError />}
+        icon={<BiCheckCircle />}
         title={`${form.getValues('groupName')} 그룹을 생성했습니다!`}
         description="멤버를 초대해 다양한 그룹 활동을 즐겨보세요"
       />
