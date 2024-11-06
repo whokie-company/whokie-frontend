84ca6ca5a166628398e89f977fdfd2237cf6799f
anheejeong <eyrt6973@naver.com>
2024-11-06 16:11:03 +0900

    Feat: createdAt store 저장

--- src/stores/selected-question.ts
+++ src/stores/selected-question.ts
@@ -3,19 +3,25 @@ import { create } from 'zustand'
 interface SelectedQuestionProps {
   questionId: number | undefined
   questionContent: string | undefined
+  questionCreatedAt: string | undefined
   setQuestionId: (questionId: number | undefined) => void
   setQuestionContent: (questionContent: string | undefined) => void
+  setQuestionCreatedAt: (questionCreatedAt: string | undefined) => void
 }
 
 export const useSelectedQuestionStore = create<SelectedQuestionProps>(
   (set) => ({
     questionId: undefined,
     questionContent: undefined,
+    questionCreatedAt: undefined,
     setQuestionId: (questionId) => {
       set({ questionId })
     },
     setQuestionContent: (questionContent) => {
       set({ questionContent })
+    },
+    setQuestionCreatedAt: (questionCreatedAt) => {
+      set({ questionCreatedAt })
     },
   })
 )
