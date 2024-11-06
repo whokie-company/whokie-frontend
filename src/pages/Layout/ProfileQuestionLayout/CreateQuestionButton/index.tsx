abfe2248fa8af5ad60a708b12e28b3659246901b
anheejeong <eyrt6973@naver.com>
2024-11-07 03:12:58 +0900

    Feat: 프로필 질문 추가하기 API 연결 및 기능 구현

---
+++ src/pages/Layout/ProfileQuestionLayout/CreateQuestionButton/index.tsx
@@ -0,0 +1,128 @@
+import { useState } from 'react'
+import { BiError, BiPlus, BiSolidPlusCircle } from 'react-icons/bi'
+
+import {
+  Box,
+  Button,
+  Flex,
+  Text,
+  Textarea,
+  useDisclosure,
+} from '@chakra-ui/react'
+import { useMutation } from '@tanstack/react-query'
+
+import { queryClient } from '@/api/instance'
+import {
+  PostProfileQuestionRequest,
+  postProfileQuestion,
+} from '@/api/services/profile/profileQuestion.api'
+import { AlertModal } from '@/components/Modal/AlertModal'
+import { FormModal } from '@/components/Modal/FormModal'
+
+type CreateQuestionButtonProps = {
+  userId: number
+}
+
+export const CreateQuestionButton = ({ userId }: CreateQuestionButtonProps) => {
+  const [newQuestionContent, setNewQuestionContent] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const formModal = useDisclosure()
+  const errorAlert = useDisclosure()
+
+  const { mutate: submitNewQuestion } = useMutation({
+    mutationFn: ({ content }: PostProfileQuestionRequest) =>
+      postProfileQuestion({ content }),
+    onSuccess: () => {
+      formModal.onClose()
+      setNewQuestionContent('')
+      queryClient.invalidateQueries({ queryKey: ['postNewQuestion'] })
+      queryClient.refetchQueries({
+        queryKey: ['profileQuestion', userId],
+      })
+    },
+    onError: () => {
+      formModal.onClose()
+      setErrorMessage('답변 전송에 실패하였습니다')
+      errorAlert.onOpen()
+    },
+  })
+
+  const handleSend = () => {
+    if (!newQuestionContent.trim()) {
+      setErrorMessage('질문을 입력해주세요')
+      formModal.onClose()
+      errorAlert.onOpen()
+      return
+    }
+
+    submitNewQuestion({
+      content: newQuestionContent,
+    })
+  }
+
+  return (
+    <Box>
+      <Button
+        colorScheme="secondary"
+        width="full"
+        position="absolute"
+        bottom={0}
+        borderRadius={0}
+        justifyContent="start"
+        paddingLeft={0}
+        onClick={formModal.onOpen}
+      >
+        <Box marginX={1.5} padding={0} color="primary_background">
+          <BiSolidPlusCircle size={22} />
+        </Box>
+        <Text fontWeight="bold" color="text">
+          질문 추가하기
+        </Text>
+      </Button>
+      <FormModal
+        isOpen={formModal.isOpen}
+        onClose={formModal.onClose}
+        icon={<BiPlus />}
+        title="프로필 질문 추가하기"
+        description="친구들에게 궁금한 질문을 추가해보세요"
+        confirmButton={
+          <Button
+            colorScheme="primary"
+            fontSize="small"
+            height="fit-content"
+            paddingY="0.6rem"
+            width="full"
+            onClick={handleSend}
+          >
+            확인
+          </Button>
+        }
+      >
+        <Flex padding="0 10px" flexDirection="column" gap={3}>
+          <Text fontSize={14}>질문</Text>
+          <Textarea
+            rows={1}
+            resize="none"
+            whiteSpace="nowrap"
+            placeholder="질문을 입력하세요"
+            value={newQuestionContent}
+            onChange={(e) => setNewQuestionContent(e.target.value)}
+          />
+          <Box textAlign="center">
+            <Text fontSize="small" textColor="text_description">
+              추가한 질문은 수정할 수 없습니다
+            </Text>
+          </Box>
+        </Flex>
+      </FormModal>
+      <AlertModal
+        isOpen={errorAlert.isOpen}
+        onClose={errorAlert.onClose}
+        icon={<BiError />}
+        title={errorMessage}
+        description=""
+      />
+    </Box>
+  )
+}
--- src/pages/Layout/ProfileQuestionLayout/QuestionSection/index.tsx
+++ src/pages/Layout/ProfileQuestionLayout/QuestionSection/index.tsx
@@ -1,12 +1,20 @@
 import { BiCookie } from 'react-icons/bi'
+import { useLocation } from 'react-router-dom'
 
 import { Box } from '@chakra-ui/react'
 
 import { PageLayout } from '@/components/PageLayout'
+import { useMyUserIdStore } from '@/stores/my-user-id'
 
+import { CreateQuestionButton } from '../CreateQuestionButton'
 import { QuestionList } from '../QuestionList'
 
 export const QuestionSection = () => {
+  const location = useLocation()
+  const userId: number = location.state?.userId
+  const myUserId = useMyUserIdStore((state) => state.myUserId)
+  const isMyPage = Number(userId) === myUserId
+
   return (
     <PageLayout.SideSection
       SectionHeader={
@@ -19,6 +27,7 @@ export const QuestionSection = () => {
       <Box fontSize="small">
         <QuestionList />
       </Box>
+      {isMyPage && <CreateQuestionButton userId={userId} />}
     </PageLayout.SideSection>
   )
 }
