f92db8a88ef532d2cbc5a5e5975e5a675768861e
anheejeong <eyrt6973@naver.com>
2024-11-08 19:40:11 +0900

    Style: 더미 데이터 삭제

--- src/pages/GroupMembersPage/Pagination/index.tsx
+++ src/pages/GroupMembersPage/Pagination/index.tsx
@@ -33,41 +33,5 @@ export default function Pagination({
         </Button>
       ))}
     </Flex>
-    // <Flex justifyContent="center" gap={3} marginTop="20px">
-    //   <Button
-    //     minWidth="32px"
-    //     height="30px"
-    //     padding="0"
-    //     border={`1px solid ${borderColor}`}
-    //     bg="none"
-    //     color="brown.600"
-    //     fontWeight="small"
-    //     _hover={{ bg: 'brown.100' }}
-    //   >
-    //     1
-    //   </Button>
-    //   <Button
-    //     minWidth="32px"
-    //     height="30px"
-    //     padding="0"
-    //     fontWeight="small"
-    //     color="white"
-    //     bg="brown.500"
-    //   >
-    //     2
-    //   </Button>
-    //   <Button
-    //     minWidth="32px"
-    //     height="30px"
-    //     padding="0"
-    //     border={`1px solid ${borderColor}`}
-    //     bg="none"
-    //     color="brown.600"
-    //     fontWeight="small"
-    //     _hover={{ bg: 'brown.100' }}
-    //   >
-    //     3
-    //   </Button>
-    // </Flex>
   )
 }
