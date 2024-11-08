6a94014c83d4809ea386cc0c7728a4ca3e610351
anheejeong <eyrt6973@naver.com>
2024-11-08 19:23:01 +0900

    Feat: 페이지네이션 포함 API 변경

--- src/api/services/group/member.api.ts
+++ src/api/services/group/member.api.ts
@@ -1,29 +1,36 @@
 import { queryOptions } from '@tanstack/react-query'
 
 import { authorizationInstance } from '@/api/instance'
-import { Member } from '@/types'
+import { appendParamsToUrl } from '@/api/utils/common/appendParamsToUrl'
+import { Member, PaginationRequestParams } from '@/types'
 
 type GroupMembersRequestParams = {
   groupId: number
-}
+} & PaginationRequestParams
 
 type GroupMembersResponse = {
-  members: Member[]
+  content: Member[]
+  totalPages?: number
+  totalElements?: number
 }
 
-const getGroupMembers = async ({ groupId }: GroupMembersRequestParams) => {
+const getGroupMembers = async (params: GroupMembersRequestParams) => {
   const response = await authorizationInstance.get<GroupMembersResponse>(
-    `/api/group/${groupId}/member`
+    appendParamsToUrl(`/api/group/${params.groupId}/member`, params)
   )
 
-  return response.data.members
+  return {
+    members: response.data.content,
+    totalPages: response.data.totalPages,
+    totalElements: response.data.totalElements,
+  }
 }
 
 export const membersQuries = {
   all: () => ['members'],
-  groupMembers: (groupId: number) =>
+  groupMembers: (groupId: number, page?: number) =>
     queryOptions({
-      queryKey: [...membersQuries.all(), groupId],
-      queryFn: () => getGroupMembers({ groupId }),
+      queryKey: [...membersQuries.all(), groupId, page],
+      queryFn: () => getGroupMembers({ groupId, page: 0, size: 5 }),
     }),
 }
