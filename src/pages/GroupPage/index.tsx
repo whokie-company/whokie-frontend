d8ea7a8802ad6b635fa0039b5ff1d88aab5cb076
Solbin Jung <jsolbin0724@gmail.com>
2024-11-09 18:34:32 +0900

    fix: 랭킹 없을 때 조건 수정

--- src/pages/GroupPage/index.tsx
+++ src/pages/GroupPage/index.tsx
@@ -51,26 +51,27 @@ const GroupSection = ({ groupId }: GroupSectionProps) => {
   return (
     <Flex flexDirection="column">
       <Profile role={role} gprofile={groupData} />
-      <Box p="0 30px">
-        <RankingGraph rank={rankingData} />
-        {rankLength && (
-          <Flex justifyContent="center" textAlign="center" margin="50px auto">
-            <Image src={sadCookie} />
-            <Box alignContent="center" marginLeft={10}>
-              <Text color="text" fontWeight="500">
-                아직
-                <Text as="span" color="orange.600">
-                  &nbsp;랭킹
-                </Text>
-                이 없어요!
+      {rankLength ? (
+        <Flex justifyContent="center" textAlign="center" margin="50px auto">
+          <Image src={sadCookie} />
+          <Box alignContent="center" marginLeft={10}>
+            <Text color="text" fontWeight="500">
+              아직
+              <Text as="span" color="orange.600">
+                &nbsp;랭킹
               </Text>
-              <Text color="text_secondary" fontSize="small">
-                친구와 함께 쿠키 주기에 참여해보세요
-              </Text>
-            </Box>
-          </Flex>
-        )}
-      </Box>
+              이 없어요!
+            </Text>
+            <Text color="text_secondary" fontSize="small">
+              친구와 함께 쿠키 주기에 참여해보세요
+            </Text>
+          </Box>
+        </Flex>
+      ) : (
+        <Box p="0 30px">
+          <RankingGraph rank={rankingData} />
+        </Box>
+      )}
       {groupId && (
         <Management
           role={role}
