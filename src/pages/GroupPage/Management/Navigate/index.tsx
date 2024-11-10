6f26eb33febeec9662576e0427d547fc2c13ed8f
anheejeong <eyrt6973@naver.com>
2024-11-10 18:51:32 +0900

    Fix: eslint 수정

--- src/pages/GroupPage/Management/Navigate/index.tsx
+++ src/pages/GroupPage/Management/Navigate/index.tsx
@@ -1,21 +1,15 @@
-import { useNavigate, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
-import { Box, Button, Text } from '@chakra-ui/react'
+import { Box, Text } from '@chakra-ui/react'
+
+import GoBack from '@/components/GoBack'
 
 export default function Navigate() {
-  const navigate = useNavigate()
   const { groupId } = useParams()
 
   return (
     <Box height="32px" display="flex" flexDirection="row">
-      <Button
-        onClick={() => navigate(`/group/${groupId}`)}
-        height="100%"
-        bg="none"
-        _hover={{ bg: 'none' }}
-      >
-        {'<'}
-      </Button>
+      <GoBack goBack={false} to={`/group/${groupId}`} />
       <Text height="100%" lineHeight="2" fontWeight="bold">
         그룹 페이지
       </Text>
