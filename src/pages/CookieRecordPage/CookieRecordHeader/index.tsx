af5bff0b766db0f24cc534df3bc7558f79575bbc
anheejeong <eyrt6973@naver.com>
2024-11-10 18:45:59 +0900

    Fix: navigate 컴포넌트로 변경

--- src/pages/CookieRecordPage/CookieRecordHeader/index.tsx
+++ src/pages/CookieRecordPage/CookieRecordHeader/index.tsx
@@ -1,14 +1,12 @@
-import { BiChevronLeft } from 'react-icons/bi'
+import { Box, Divider, Flex, Text } from '@chakra-ui/react'
 
-import { Box, Center, Divider, Flex, Text } from '@chakra-ui/react'
+import GoBack from '@/components/GoBack'
 
 export const CookieRecordHeader = () => {
   return (
     <Flex flexDirection="column">
       <Box height={8} display="flex" flexDirection="row" width="full">
-        <Center paddingX={2} _hover={{ cursor: 'pointer', color: 'black.700' }}>
-          <BiChevronLeft size={20} />
-        </Center>
+        <GoBack goBack />
         <Text height="100%" lineHeight="2" fontWeight="bold">
           쿠키 기록
         </Text>
