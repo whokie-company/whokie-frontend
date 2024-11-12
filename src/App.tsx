ed7158cd28cc64900070ff3555c6254adfb64777
ajin <aj5210@naver.com>
2024-11-12 22:49:32 +0900

    Refactor: 알림 기능 로그인 했을 때만 실행되게 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
-import { useEffect } from 'react'
-
-import { ChakraProvider, useToast } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import { QueryClientProvider } from '@tanstack/react-query'
 
 import { queryClient } from '@/api/instance'
 import { Routes } from '@/routes'
 import theme from '@/styles/theme'
 
-import { CookieAlaram } from './components/CookieAlaram'
-import { useSEEMessage } from './hooks/useSEEMessage'
-
 function App() {
-  const { message } = useSEEMessage()
-  const toast = useToast()
-
-  useEffect(() => {
-    if (message) {
-      toast.closeAll()
-      toast({
-        position: 'top',
-        duration: 3000,
-        render: () => (
-          <CookieAlaram message={message} onClickCloseButton={toast.closeAll} />
-        ),
-      })
-    }
-  }, [toast, message])
-
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme}>
--- src/routes/ProtectedRoute/index.tsx
+++ src/routes/ProtectedRoute/index.tsx
@@ -1,9 +1,30 @@
+import { useEffect } from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 
+import { useToast } from '@chakra-ui/react'
+
+import { CookieAlaram } from '@/components/CookieAlaram'
+import { useSEEMessage } from '@/hooks/useSEEMessage'
 import { useAuthTokenStore } from '@/stores/auth-token'
 
 export const ProtectedRoute = () => {
   const isLoggedIn = useAuthTokenStore((state) => state.isLoggedIn())
+
+  const { message } = useSEEMessage()
+  const toast = useToast()
+
+  useEffect(() => {
+    if (isLoggedIn && message) {
+      toast.closeAll()
+      toast({
+        position: 'top',
+        duration: 3000,
+        render: () => (
+          <CookieAlaram message={message} onClickCloseButton={toast.closeAll} />
+        ),
+      })
+    }
+  }, [toast, message, isLoggedIn])
 
   return isLoggedIn ? <Outlet /> : <Navigate to="/login" replace />
 }
